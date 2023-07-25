b90734f08a796d402e2d5df684be4a09c9266c07
Debora Damas <damasdebora32@gmail.com>
2023-07-25 13:53:09 -0300

    show busy rooms

--- Hostel/src/main/java/com/mycompany/hostel/Hostel.java
+++ Hostel/src/main/java/com/mycompany/hostel/Hostel.java
@@ -23,9 +23,9 @@ public class Hostel {
         
         System.out.println("How many rooms will be rented? ");
         int n = sc.nextInt();
-        
-        
+         
         for(int i = 1; i <= n; i += 1){
+            System.out.println(""); //line break
             System.out.println("Rent #" + i + ":");
             System.out.print("Name: ");
             sc.nextLine();
@@ -38,6 +38,14 @@ public class Hostel {
             int room = sc.nextInt();
 
             rooms[room] = new Rent(name, email);
+        }
+        
+        System.out.println(""); //line break
+        System.out.println("Busy rooms: ");
+        for(int i =0; i < 10; i ++){
+            if(rooms[i] != null){
+                System.out.println(i + ": " +rooms[i]);
+            }
         }
         
         sc.close();           
