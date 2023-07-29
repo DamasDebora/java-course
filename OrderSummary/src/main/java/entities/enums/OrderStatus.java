4a2a9e501cb8d7d00e0a2c8379c5512f6ab866f1
Debora Damas <damasdebora32@gmail.com>
2023-07-29 17:38:21 -0300

    order system

---
+++ OrderSummary/src/main/java/entities/enums/OrderStatus.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package entities.enums;
+
+import javax.annotation.processing.ProcessingEnvironment;
+
+/**
+ *
+ * @author debora
+ */
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
