de4ce97fcc30099c7cc4538bbab3dd2f606fc519
Debora Damas <damasdebora32@gmail.com>
2023-07-31 14:09:06 -0300

    rectangle class

--- Shapes/src/main/java/entities/Rectangle.java
+++ Shapes/src/main/java/entities/Rectangle.java
@@ -4,10 +4,47 @@
  */
 package entities;
 
+import entities.enums.Color;
+
 /**
  *
  * @author debora
  */
-public class Rectangle {
+public class Rectangle extends Shape{
+    private Double width;   
+    private Double height; 
+
+    public Rectangle() {
+        super();
+    }
+
+    public Rectangle(Double height, Double width, Color color) {
+        super(color);
+        this.height = height;
+        this.width = width;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
+    
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+    
+    
+
+    @Override
+    public double area() {
+        return width * height;
+    }
     
 }
