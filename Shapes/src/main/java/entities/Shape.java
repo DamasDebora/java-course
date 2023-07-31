cd7344b3174d88c9986bd2a410095095b123b679
Debora Damas <damasdebora32@gmail.com>
2023-07-31 14:02:01 -0300

    Shape class

--- Shapes/src/main/java/entities/Shape.java
+++ Shapes/src/main/java/entities/Shape.java
@@ -4,10 +4,29 @@
  */
 package entities;
 
+import entities.enums.Color;
+
 /**
  *
  * @author debora
  */
-public class Shape {
-    
+public abstract class Shape {
+    private Color color;
+
+    public Shape() {
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+  
+    public abstract Double area();
 }
