e2c767c80951b4ff383afc1bf06b75b8b690d961
Debora Damas <damasdebora32@gmail.com>
2023-08-01 11:19:52 -0300

    initial structure

---
+++ HotelReservations/src/main/java/com/mycompany/hotelreservations/HotelReservations.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelreservations;
+
+/**
+ *
+ * @author debora
+ */
+public class HotelReservations {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
