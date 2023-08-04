0abe2571ac153b8cd33187a8e141c9bc82d2fe4b
Debora Damas <damasdebora32@gmail.com>
2023-08-04 16:06:12 -0300

    services

---
+++ carRent/src/main/java/model/services/RentalServices.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.services;
+
+import java.time.Duration;
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+/**
+ *
+ * @author debora
+ */
+public class RentalServices {
+    
+    private Double pricePerHor;
+    private Double pricePerDay;
+    private BrazilTaxServices taxService;
+
+    public RentalServices(Double pricePerHor, Double pricePerDay, BrazilTaxServices taxService) {
+        this.pricePerHor = pricePerHor;
+        this.pricePerDay = pricePerDay;
+        this.taxService = taxService;
+    }
+    
+    public void process(CarRental carRental){
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+        
+        double basicPayment;
+        if(hours <= 12.0){
+            basicPayment = pricePerHor * Math.ceil(hours);
+        } else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+        }
+        
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
